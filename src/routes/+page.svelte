8bae56fc217e300620c54969482049ff34cab6a6
Kevin <50965376+tgodkev@users.noreply.github.com>
2022-11-14 23:28:36 -0500

    style changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { fly } from "svelte/transition";
   /** @type {import('./$types').PageData } */
   export let data;
 
@@ -6,27 +7,32 @@
   $: ({ phones, laptops, decor } = data);
 
   let showInfo = [];
-  console.log(phones, "<---- phones");
+  console.log(phones.products, "<---- phones");
 </script>
 
 <section class="py-44">
-  <div class="flex justify-center">
+  <div class="flex justify-start px-5 md:px-20">
     <h2 class="text-white text-4xl font-bold pb-10">Phones</h2>
   </div>
-  <div class="flex overflow-auto md:flex-wrap md:justify-center ">
+  <div
+    class="flex overflow-auto md:flex-wrap md:justify-center md:overflow-hidden   "
+  >
     {#each phones.products as phone, i}
       <div
         on:mouseenter={() => (showInfo[i] = !showInfo[i])}
         on:mouseleave={() => (showInfo[i] = !showInfo[i])}
         on:focus={() => (showInfo[i] = !showInfo[i])}
-        class="hover:shadow-md hover:shadow-white my-2 mx-10 border-[2px] border-white p-20"
+        class=" md:overflow-hidden"
       >
         <div
-          class="flex flex-col items-center  h-[25rem] w-[25rem] bg-cover bg-center"
-          style="background-image: url({phone.images[0]});"
+          class="flex flex-col items-center rounded-2xl  h-[25rem] w-[25rem] bg-cover bg-center hover:shadow-md hover:shadow-white my-2 mx-2"
+          style="background-image: url({phone.images[2]});"
         >
           {#if showInfo[i]}
-            <div>
+            <div
+              in:fly={{ y: 700, duration: 900 }}
+              out:fly={{ y: -700, duration: 900 }}
+            >
               <h3 class="text-2xl font-bold text-white">{phone.title}</h3>
               <p class="text-xl font-bold text-black">{phone.description}</p>
               <p class="text-xl font-bold text-white">${phone.price}</p>
